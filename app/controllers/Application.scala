898ac2eacbf14b74e425c03097e24f4985c007ca
Faissal Boutaounte <b.faissal@gmail.com>
2014-01-20 18:14:04 -0500

    added comments to Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,15 +12,26 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  // initiate the broadcas, that will be used to push data
   val (enumerator,channel) = Concurrent.broadcast[String]
 
+  // get you OAuth config from you twitter dev account : https://dev.twitter.com
   val requestToken = RequestToken("access token","access token secret")
   val consumerKey = ConsumerKey("consumer key","consumer secret")
-  WS.url("https://stream.twitter.com/1.1/statuses/filter.json?track=qsd").withRequestTimeout(-1)
+  // make a get request to twitter streaming API
+  WS.url("https://stream.twitter.com/1.1/statuses/filter.json?track=qsd")
+    // no time out as it's an infinite stream ( we are talking big big data here ;) )
+    .withRequestTimeout(-1)
     .sign(new OAuthCalculator(consumerKey, requestToken))
+    // every time we get somthing from twitter we push it into our websocket channel
     .get { headers => Iteratee.foreach[Array[Byte]]( bytes => channel.push(new String(bytes))) }.map(_.run)
-
+  /* this is how websockets work inside play framework: http://www.playframework.com/documentation/2.2.x/ScalaWebSockets
+      * TL;DR:
+      * in: Itratee = is a consumer you can use it to handle mssage sent from you clients
+      * enumerator : Enumerator = is a message producer
+     */
   def wsTweet = WebSocket.using[String] { request =>
+    // do nothing when we got a websocket messae from one of the connected client
     val in = Iteratee.foreach[String]( msg => { () })
     (in, enumerator)
   }
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,12 +1,9 @@
-//angular.module("jmaghreb",[]).controller("tweetWall",
 angular.module('jmaghreb',[]).factory('WSocket', ['$rootScope','$location', function ($rootScope,$location) {
     var WSocket = {};
     WSocket.ws = new WebSocket('ws://'+$location.host()+':'+$location.port()+'/wsTweet');
-
     WSocket.ws.onopen = function(){
         console.log("Socket has been opened!");
     };
-
     WSocket.ws.onmessage = function(message) {
         $rootScope.$apply(function(){
             $rootScope.addTweet(JSON.parse(message.data))
@@ -18,12 +15,10 @@ angular.module('jmaghreb',[]).factory('WSocket', ['$rootScope','$location', func
 
 function tweetWall($scope,$rootScope,WSocket,$location){
     $rootScope.tweets = []
-
     $rootScope.addTweet = function(data){
         $rootScope.tweets.unshift( data)
         if($rootScope.tweets.length > 5 ){
             $rootScope.tweets.pop()
         }
-
     }
 }
